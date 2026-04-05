92cd807163a88e364bc278121f03bfb71d49ceed
Moisei-D <daniel.moisei@isa.utm.md>
2026-04-05 19:17:47 +0300

    Added WeightedGraph class and Generator

---
+++ Laboratories/Lab4/Graphs/WeightedGraph.cs
@@ -0,0 +1,43 @@
+namespace Lab4.Graphs;
+
+public class WeightedGraph : IGraph
+{
+    private readonly List<(int neighbor, double weight)>[] _adj;
+
+    public int VertexCount { get; }
+
+    public WeightedGraph(int vertexCount)
+    {
+        VertexCount = vertexCount;
+        _adj = new List<(int, double)>[vertexCount];
+        for (int i = 0; i < vertexCount; i++)
+        {
+            _adj[i] = new List<(int, double)>();
+        }
+    }
+
+    public void AddEdge(int u, int v, double weight)
+    {
+        _adj[u].Add((v, weight));
+        _adj[v].Add((u, weight)); // undirected
+    }
+
+    public IReadOnlyList<(int neighbor, double weight)> GetNeighbors(int vertex)
+        => _adj[vertex];
+
+    public double[,] ToAdjacencyMatrix()
+    {
+        double inf = double.PositiveInfinity;
+        var matrix = new double[VertexCount, VertexCount];
+        for (int i = 0; i < VertexCount; i++)
+            for (int j = 0; j < VertexCount; j++)
+                matrix[i, j] = (i == j) ? 0.0 : inf;
+
+        for (int u = 0; u < VertexCount; u++)
+            foreach (var (v, w) in _adj[u])
+                matrix[u, v] = Math.Min(matrix[u, v], w);
+
+        return matrix;
+    }
+
+}
